e91e6546f7cb7ac9eb9d164d7d3169340bf9d29e
RodrigoMenegat <rodrigoschuinski@gmail.com>
2020-10-02 21:30:24 -0300

    Adiciona variáveis da thread de 7d ao tweet

--- code/process_tweet_variables.py
+++ code/process_tweet_variables.py
@@ -35,12 +35,14 @@ DF_FULL = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/tilesets/bd_completo
 CONSERVATION_UNITS_FIRE_DATA = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/land_info/unidades_de_conservacao.feather")
 INDIGENOUS_LAND_FIRE_DATA = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/land_info/terras_indigenas.feather")
 BIOMES_FIRE_DATA = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/land_info/biomas.feather")
+GRID_FIRE_DATA = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/land_info/grid_20km.feather")
 
 CITIES = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/sources/cidades_amazonia_legal.feather")
 CONSERVATION_UNITS =  gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/sources/unidades_de_conservacao.feather")
 INDIGENOUS_LAND = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/sources/terras_indigenas.feather")
 LEGAL_AMAZON = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/sources/limites_amazonia_legal.feather")
 BIOMES = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/sources/biomas_amazonia_legal.feather")
+GRID = gpd.read_feather(f"{PROJECT_ROOT}/output/feathers/sources/grid_20km.feather")
 
 ###############
 ### Helpers ###
@@ -81,6 +83,7 @@ def look_up(df, lookup_column, lookup_value, result_column):
     else:
         raise ValueError("Mais de um resultado encontrado. Assegure-se que o código para busca não é ambíguo.")
 
+
 def find_places_with_fire(df, code):
     '''
     Conta quantas unidades do tipo de terra selecionado
@@ -101,6 +104,7 @@ def find_places_with_fire(df, code):
 
 
     return terras_com_fogo
+
 
 def find_place_with_most_fire(df, code, total_fires, position=1):
     '''
@@ -158,6 +162,7 @@ def find_place_with_most_fire(df, code, total_fires, position=1):
         "dias_consecutivos": dias_consecutivos,
         "estados": estados
     }
+
 
 def burning_for_the_longest(df, code, position=1):
     '''
@@ -231,11 +236,12 @@ def total_fires(df, code):
 ### Funções principais ###
 ##########################
 
-def find_values():
+def find_values_24h():
     '''
     Faz consulta aos bancos de dados para gerar todas as
-    variáveis necessárias para a elaboração do fio no
-    Twitter.
+    variáveis necessárias para a elaboração dos fios
+    que são publicados a cada 24h no Twitter.
+    Eles resumem o estado 
     '''
     
     # 1. Thread de terras indígenas nas últimas 24h
@@ -277,9 +283,6 @@ def find_values():
     unidades_de_conservacao["areas_fogo_mais_dias"] = { }
     for i in range(1,4):
         unidades_de_conservacao["areas_fogo_mais_dias"][f"{i}"] = burning_for_the_longest(CONSERVATION_UNITS_FIRE_DATA, 'cod_uc', i)
-
-
-    # 3. O total de fogos na Amazônia Legal e
     
 
 
@@ -290,15 +293,135 @@ def find_values():
     }
 
 
+def find_values_7d():
+    '''
+    Faz consulta aos bancos de dados para gerar todas as
+    variáveis necessárias para a elaboração do fio no
+    Twitter.
+    '''
+
+    ###############
+    ### Helpers ###
+    ###############
+
+    def find_rects_with_fire_in_proteced_areas(n, time):
+        '''
+        Descobre, dos n grids com mais fogo, quantos estão
+        em Terras Indígenas e Unidades de Conservação.
+
+        Parâmetros:
+
+        n -> Inteiro que representa quantidade de grids que devem ser considerados nessa conta. 
+        time -> String que representa o intervalo de agregação. Pode ser '24h', '7d' ou 'full_db'
+        '''
+
+        # Seleciona os dez grids com mais foco
+        grids_most_fire = GRID_FIRE_DATA.sort_values(by=f"focos_{time}").head(n)
+
+        # Quantos desses estão em terras indígenas?
+        grids_ind_land = grids_most_fire[~grids_most_fire.cod_ti.isna()].shape[0]
+
+        # Quantos desses estão em unidades de conservação?
+        grids_conservation_unit = grids_most_fire[~grids_most_fire.cod_uc.isna()].shape[0]
+
+
+        return {
+            "in_indigenous_land": grids_ind_land,
+            "in_conservation_units": grids_conservation_unit,
+            "total": grids_ind_land + grids_conservation_unit
+        }
+
+
+
+    def find_grid_with_most_fire(time, total_fires, position=1):
+        '''
+        Encontra qual é o retângulo do grid com mais fogo
+        em um determinado recorte temporal. Retorna, também,
+        informações detalhadas sobre ele.
+        
+        Parâmetros:
+
+        time -> String que representa o intervalo de agregação. Pode ser '24h', '7d' ou 'full_db'
+
+        total_fires -> O número de focos de incêndio total registrados em áreas do mesmo tipo. É usado
+        para calcular qual o percentual que os focos registrados em cada terra representam do total.
+
+        position -> Com esse parâmetro, é possível selecionar outros locais que não o primeiro colocado em número
+        de focos. Por exemplo, se especificarmos o valor 2, a função vai retornar dados sobre o segundo território
+        em situação mais crítica.
+        '''
+        
+        # Seleciona o grid com mais fogo
+        grid_most_fire = GRID_FIRE_DATA.sort_values(by=f"focos_{time}", ascending = False).reset_index().loc[position-1]
+        
+        # Pega as informações básicas
+        id_ = grid_most_fire["cod_box"]
+        n_focos = int(grid_most_fire[f"focos_{time}"])
+
+        porcentagem = round(n_focos / total_fires * 100)
+        dias_consecutivos = grid_most_fire["dias_consecutivos"]
+        
+        # Vamos escolher apenas um dos vários territórios que intersectam a área para garantir que o tuíte seja sucinto
+        cidade = grid_most_fire["cidade"].split(",")[0].strip() if grid_most_fire["cidade"] else None
+        estado = grid_most_fire["estado"].split(",")[0].strip() if grid_most_fire["estado"] else None
+        nome_ti = grid_most_fire["nome_ti"].split(",")[0].strip() if grid_most_fire["nome_ti"] else None
+        cod_ti = grid_most_fire["cod_ti"].split(",")[0].strip() if grid_most_fire["cod_ti"] else None
+        nome_uc = grid_most_fire["nome_uc"].split(",")[0].strip() if grid_most_fire["nome_uc"] else None
+        cod_uc = grid_most_fire["cod_uc"].split(",")[0].strip() if grid_most_fire["cod_uc"] else None
+        nome_bioma = grid_most_fire["nome_bioma"].split(",")[0].strip() if grid_most_fire["nome_bioma"] else None
+
+        # Pega o nome curto, pré-formatado, da UC
+        if cod_uc:
+            nome_uc_curto = CONSERVATION_UNITS[CONSERVATION_UNITS.cod_uc == cod_uc].reset_index().loc[0, "nome_uc_curto"]
+        else:
+            nome_uc_curto = None
+        
+        
+        return {
+            "id": id_,
+            "n_focos": n_focos,
+            "porcentagem": porcentagem,
+            "dias_consecutivos": dias_consecutivos,
+            "cidade": cidade,
+            "estados": estado,
+            "nome_ti": nome_ti,
+            "nome_uc": nome_uc,
+            "nome_uc_curto": nome_uc_curto,
+            "nome_bioma": nome_bioma
+        }
+
+
+
+    # Áreas do grid com mais fogo
+    grid = {}
+
+    # Das dez áreas com mais fogo, quantas são em UCs e TIs?
+    grid["fire_in_protected_areas"] = find_rects_with_fire_in_proteced_areas(10, "7d")
+
+    # Grids com mais focos de foco nos últimos 7d e detalhes sobre eles
+    grid["areas_mais_fogo_7d"] = { }
+    for i in range(1, 4):
+        grid["areas_mais_fogo_7d"][f"{i}"] = find_grid_with_most_fire("7d", DF_7D.shape[0], i)
+
+    return {
+        "total_focos_amazonia_legal_2020": DF_FULL.shape[0],
+        "total_focos_7d": DF_7D.shape[0],
+        "grid": grid,
+    }
+
 ################
 ### Execução ###
 ################
 
 def main():
 
-	data = find_values()
-	with open(f"{PROJECT_ROOT}/output/jsons/alerts/24h.json", "w+") as f:
-		json.dump(data, f, indent=4)
+    data = find_values_24h()
+    with open(f"{PROJECT_ROOT}/output/jsons/alerts/24h.json", "w+") as f:
+        json.dump(data, f, indent=4)
+
+    data = find_values_7d()
+    with open(f"{PROJECT_ROOT}/output/jsons/alerts/7d.json", "w+") as f:
+        json.dump(data, f, indent=4)
 
 
 if __name__ == "__main__":
