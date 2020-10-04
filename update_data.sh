5db976452c01851e72fa0cf778c4c6e691b35fd2
RodrigoMenegat <rodrigoschuinski@gmail.com>
2020-10-04 18:50:06 -0300

    Adiciona sleep para dar tempo do Mapbox processar alterações no tileset

--- update_data.sh
+++ update_data.sh
@@ -2,6 +2,7 @@ source /root/anaconda3/etc/profile.d/conda.sh &&
 conda activate amazonia_sufocada &&
 cd /home/amazonia-sufocada/code/ &&
 python update_datasets.py &&
+sleep 30m && # Sleep para processamento dos tilesets
 python update_tweet_data.py &&
 python tweet.py "../output/jsons/tweets/ucs_24h.json" &&
 sleep 60m &&
