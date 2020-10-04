e7f8b52dab6ef22c2277651d7808ede60229b7c9
Rodrigo Menegat <rodrigo@meneg.at>
2020-10-04 21:55:02 +0000

    Path absoluto para tweets

--- update_data.sh
+++ update_data.sh
@@ -3,6 +3,6 @@ conda activate amazonia_sufocada &&
 cd /home/amazonia-sufocada/code/ &&
 python update_datasets.py &&
 python update_tweet_data.py &&
-python tweet.py "../output/jsons/tweets/ucs_24h.json" &&
+python tweet.py "/home/amazonia-sufocada/output/jsons/tweets/ucs_24h.json" &&
 sleep 60m &&
-python tweet.py "../output/jsons/tweets/tis_24h.json"
+python tweet.py "/home/amazonia-sufocada/output/jsons/tweets/tis_24h.json"
