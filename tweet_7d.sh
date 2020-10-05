3a62772bfa29d13681f84da1d416dc1f7c44cbeb
Rodrigo Menegat <rodrigo@meneg.at>
2020-10-04 21:43:56 -0300

    Adiciona bash script para fio semanal

---
+++ tweet_7d.sh
@@ -0,0 +1,5 @@
+source /root/anaconda3/etc/profile.d/conda.sh &&
+conda activate amazonia_sufocada &&
+cd /home/amazonia-sufocada/code/ &&
+python tweet.py "/home/amazonia-sufocada/output/jsons/tweets/grid_7d.json"
+
